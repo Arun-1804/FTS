6efc75ee31d5dc02f75b22bdde32ec0cbd98b591
Arun-1804 <118242526+Arun-1804@users.noreply.github.com>
2024-09-04 23:36:57 +0530

    Add files via upload

--- src/pages/Dasboard.js
+++ src/pages/Dasboard.js
@@ -1,11 +1,41 @@
 import crackImage from "../assests/crack.jpg"
 import dampimage from "../assests/damp.jpg"
 export default function DasboardPage(){
+
+  const postfunc = () => {
+    // let json = {ip:'khfwiyf89iug'}
+    alert("post")
+    // fetch(`http://127.0.0.1:5000/sendIp`, {
+    fetch(`http://127.0.0.1:5000/sendIp?ip=${'http://192.168.251.58:8080/video'}`, {
+      method: "GET",
+      mode: "no-cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body:JSON.stringify(json)
+    })
+      .then((res) => {
+         console.log(res.json())
+         return false;
+        // code = res.status;
+        // message = res.statusText;
+        // if (res.status == 200) {
+          // return res?.json();
+          // }
+        })
+        // .then((res) => {
+        //  console.log(res);
+        //  return false;
+         
+        // })
+  }
     return (
         <> <div className="min-h-screen bg-gray-50 flex flex-col justify-center relative overflow-hidden sm:py-12" style={{backgroundColor:"grey"}}> <div className="flex items-center justify-around">
         
         <div className="flex items-center">
-          <button>
+          <button onClick={postfunc}>
 
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
         <img 
